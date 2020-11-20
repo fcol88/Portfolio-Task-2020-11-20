07e817636e7a6d16c6058668f0ab5a71b3a698a3
Matt Graham <m.j.graham@live.co.uk>
2020-11-20 13:04:05 +0000

    adding iterable table and stub task form

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from task_list.tasks.TaskList import TaskList
 from task_list.users.User import User
 from task_list.users.Admin import Admin
+import babel
 
 app = Flask(__name__)
 app.debug = True
@@ -10,8 +11,28 @@ tasks = TaskList()
 regularUser = User("Regular", "User", 1234)
 adminUser = Admin("Admin", "User", 4246)
 
+@app.template_filter('datetime')
+def format_datetime(value, format='short'):
+    if format == 'long':
+        format="EEEE, d. MMMM y 'at' HH:mm"
+    elif format == 'medium':
+        format="EE dd.MM.y HH:mm"
+    return babel.dates.format_datetime(value, format)
+
 @app.route("/tasklist")
-def register():
-    return render_template('task_list.html')
+def tasklist():
+
+    return render_template('task_list.html', title="Task List", tasks=tasks)
+
+@app.route("/addtask", methods=["GET", "POST"])
+def add_task():
+
+    if request.method == "POST":
+
+        #do something
+
+        return redirect('/tasklist')
+
+    return render_template('add_task.html')
 
 app.run()
