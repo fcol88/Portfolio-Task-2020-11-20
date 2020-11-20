0c6fb6c28675751db03865e81b12835b58fe26d0
Matt Graham <m.j.graham@live.co.uk>
2020-11-20 12:10:17 +0000

    bringing in task list package

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+from task_list.tasks.TaskList import TaskList
+from task_list.users.User import User
+from task_list.users.Admin import Admin
 
 app = Flask(__name__)
 app.debug = True
+
+tasks = TaskList()
+regularUser = User("Regular", "User", 1234)
+adminUser = Admin("Admin", "User", 4246)
 
 @app.route("/register")
 def register():
